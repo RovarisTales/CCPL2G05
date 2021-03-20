96f175e1394c5681958379ca78fd8af08db59058
PauloPinto2146 <79594633+PauloPinto2146@users.noreply.github.com>
2021-03-20 17:11:59 +0000

    Update parser.c

--- parser.c
+++ parser.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "parser.h"
-
 
 void parser (char *line){
     char *delimit = " /n/t";
@@ -11,7 +11,31 @@ void parser (char *line){
         char *sobra;
         long valint = strtol(token; &sobra; 10);
         if (strlen(sobra) == 0){
-            PUSH(valint)
+            PUSH(valint);
+}else if (strcmp(token, "-") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (X - Y);
+} else if (strcmp(token, "+") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (X + Y);
+} else if (strcmp(token, "*") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (X * Y);
+} else if (strcmp(token, "/") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (X / Y);
+} else if (strcmp(token, "#") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (pow (X,Y));
+} else if (strcmp(token, "%") == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        PUSH (X % Y);
         }
     }
 }

9401f155af84b5ec96fe3afcd986a2bd030094e1
Tsushiko <a97509@alunos.uminho.pt>
2021-03-20 16:37:26 +0000

    Create Parser.c

---
+++ Parser.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main (){
+    char line[10240];
+    assert(fgets(line,10240,stdin)!=Null);
+    assert(line[strlen(line)- 1]== '\n');
+    parser(line);
+    return 0;
+}

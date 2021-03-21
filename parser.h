9e6dd8678d66ef09f96a6585626204d9f27bcb0c
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-03-21 19:50:01 +0000

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,9 +1,6 @@
 void parser (char *line);
 
-void inverteArray (int v[], int N);
-
 void convertebinario(long x, int a[]);
+
 int convertedecimal(int a[]);
 
-void swapM (int *x, int *y);
-void swap(int v[], int i, int j);

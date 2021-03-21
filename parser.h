df30d07787b2d483eaada24b155ea80f1b201105
RovarisTales <79594953+RovarisTales@users.noreply.github.com>
2021-03-21 22:30:09 +0000

    Update parser.h

--- parser.h
+++ parser.h
@@ -4,3 +4,15 @@ void convertebinario(long x, int a[]);
 
 int convertedecimal(int a[]);
 
+void swapM (int *x, int *y);
+
+void swap (int v[], int i, int j);
+
+void inverteArray (int v[], int N);
+
+
+
+
+
+
+

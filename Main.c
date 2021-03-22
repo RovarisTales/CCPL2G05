485c03552775fe8cdfc74076f141a76123d73406
RovarisTales <79594953+RovarisTales@users.noreply.github.com>
2021-03-22 16:20:24 +0000

    Update Main.c

--- Main.c
+++ Main.c
@@ -6,7 +6,7 @@
 
 int main (){
     char line[10240];
-    assert(fgets(line,10240,stdin)!=Null);
+    assert(fgets(line,10240,stdin)!=NULL);
     assert(line[strlen(line)- 1]== '\n');
     parser(line);
     return 0;

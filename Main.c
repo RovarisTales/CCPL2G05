36c667a64664d42a7e3b2aa3f9a11a46786dd3ec
miguelrego25 <61994815+miguelrego25@users.noreply.github.com>
2021-03-24 18:40:55 +0000

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,9 +1,18 @@
+/**
+ * @file Ficheiro que contém a função main do programa.
+ * Esta função é a função inicial do programa que contem a função parse do modulo parser.h
+  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
 #include "parser.h"
-
+/**
+ * \brief Função principal do programa
+ * Esta função le um input introduzido pelo ser misterioso e realiza a função parser.
+ *
+ * @return 0
+ */
 int main (){
     char line[10240];
     assert(fgets(line,10240,stdin)!=NULL);

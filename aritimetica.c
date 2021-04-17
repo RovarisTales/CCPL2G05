7ef56dd6a811b4f27fbb0986c92e16495465aeef
Tales <tales.rovaris@hotmail.com>
2021-04-17 12:44:24 +0100

    separaei aritimetica em um modulo

---
+++ aritimetica.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "stack.h"
+#include "parser.h"
+
+void aritimetica (char *oper){
+    if (strcmp(oper, "-") == 0) {
+        char A = POPT();
+        double  Y = POP ();
+        char B = POPT();
+        double  X = POP ();
+        A = comparatipo (A,B);
+        PUSH (X - Y,A);
+    } else if (strncmp(oper, "+",1) == 0) {
+        char A = POPT();
+        double  Y = POP ();
+        char B = POPT();
+        double  X = POP ();
+        A = comparatipo (A,B);
+        PUSH (X + Y,A);
+    } else if (strncmp(oper, "*", 1) == 0) {
+        char A = POPT();
+        double  Y = POP ();
+        char B = POPT();
+        double  X = POP ();
+        double a = X*Y;
+        A = comparatipo (A,B);
+        PUSH (a,A);
+    } else if (strncmp(oper, "/",1) == 0) {
+        char A = POPT();
+        double  Y = POP ();
+        char B = POPT();
+        double  X = POP ();
+        A = comparatipo(A,B);
+        double T = X / Y;
+        T = convertelong (T,A);
+        
+        PUSH (T,A);
+    } else if (strncmp(oper, "#",1) == 0) {
+        double  Y = POP ();
+        double  X = POP ();
+        PUSH (pow (X,Y),'f');
+    } else if (strncmp(oper, "%",1) == 0) {
+        long  Y = POP ();
+        long  X = POP ();
+        
+        PUSH (X % Y,'i');
+    } else if (strncmp(oper, "(",1) == 0) {
+        char A = POPT();
+        double  X = POP ();
+        X = X - 1;
+        PUSH (X,A);
+    } else if (strncmp(oper, ")",1) == 0) {
+        char A = POPT();
+        double  X = POP ();
+        X = X + 1;
+        PUSH (X,A);
+    }
+
+
+}
+
+
+
+
---
+++ aritimetica.h
@@ -0,0 +1,1 @@
+void aritimetica (char *oper);

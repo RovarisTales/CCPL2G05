66a9db34645337fc81027cb0fa9a48cb5df5b8b0
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-05-08 19:31:25 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -27,16 +27,58 @@ struct Stack{
     int top;
 };
 
+/**
+ * \brief Esta funçao verifica se a SPointer se encontra vazia.
+ * @param s Pointer para a stack principal
+ */
+
 int vazio(SPointer s);
-int cheio(SPointer s);
+
+/**
+ * \brief Esta funçao remove o elemento no topo da stack
+ * @param s Pointer para s stack principal
+ */
 Tipoval POP(SPointer s);
+
+/**
+ * \brief Esta funçao adiciona o elemento colocado como argumento para o topo da stack
+ * @param s Pointer para a stack principal
+ * @param a Valor que queremos colocar na stack
+ */
 void PUSH(Tipoval a,SPointer s);
+
+/**
+ * \brief Esta funçao que imprime todos os elementos da stack
+ * @param s Pointer para s stack principal
+ */
 void print_stack(SPointer s);
+
+/**
+ * \brief Esta funçao move o top do pointer da stack para uma posição especifica
+ * @param s Pointer para s stack principal
+ * @param a Posição para onde vamos deslocar.
+ */
 void MOVE (int a,SPointer s);
+
+/**
+ * \brief Esta função serve para retirar um elemento numa posição espefica na stack.
+ * @param a Posição onde queremos retirar o elemento.
+ * @param s Pointer para a stack principal
+ */
 Tipoval MOVEPOP (int a,SPointer s);
+
+/**
+ *\brief Como o próprio nome indica cria uma stack 
+ * @param s Pointer para a stack
+ * @param tamanho Tamanho da stack que queremos criar
+ * return s Retorna a stack criada
+ */
 SPointer criaStack(SPointer s,int tamanho);
-void PUSHARRAY (Tipoval a, SPointer s);
-Tipoval POPFALSO(SPointer s);
-Tipoval POPFALSO2(SPointer s);
+
+/**
+ * \brief Esta função copia a stack 
+ * @param x Para onde vamos colocar a copia da stack
+ * @param s Pointer para a stack principal
+ */
 SPointer copystack(SPointer s,SPointer x);
 

89f927dbe3223cf872d4e3af84742532a8c44d2b
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-05-08 16:45:20 +0100

    Update aritimetica.h

--- aritimetica.h
+++ aritimetica.h
@@ -2,4 +2,10 @@
  * @file É o ficheiro que contem a head do ficheiro aritimetica.c
  *
  */
+
+/**
+ * \brief header da função principal do ficheiro aritimetica.c
+ * @param oper Token dentro dos mencionados no parser ("+*-/#%()")
+ * @param s pointer para a stack
+ */
 void aritimetica (char *oper, SPointer s);

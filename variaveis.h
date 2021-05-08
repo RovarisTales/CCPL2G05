5e3848086d46ef30c422fef69f3d71e877a70e79
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-05-08 19:52:43 +0100

    Update variaveis.h

--- variaveis.h
+++ variaveis.h
@@ -3,4 +3,10 @@
  *
  */
 
+/**
+ * \brief Função que vai reencaminhar para as suas subfunções (variaveisA (com :) e variaveisB(sem :))
+ * @param token dentro dos mencionados no parser (":ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+ * @param alfabeto Letra do alfabeto em maiuscula que corresponde ao codigo ascii (65-90)
+ * @param s Pointer para a stack
+ */
 void variaveis(char *token, Tipoval *alfabeto,SPointer s);

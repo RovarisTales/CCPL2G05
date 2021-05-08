1cdb9ff9be6c0b60240d846457f3336afcfcb811
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-05-08 20:55:45 +0100

    Update stack.h

--- stack.h
+++ stack.h
@@ -35,55 +35,54 @@ struct Stack{
 };
 
 /**
- * \brief Esta funçao verifica se a SPointer se encontra vazia.
+ * \brief Head da função vazio que se encontra em stack.c
  * @param s Pointer para a stack principal
  */
 
 int vazio(SPointer s);
 
 /**
- * \brief Esta funçao remove o elemento no topo da stack
+ * \brief Head da função POP que se encontra em stack.c
  * @param s Pointer para s stack principal
  */
 Tipoval POP(SPointer s);
 
 /**
- * \brief Esta funçao adiciona o elemento colocado como argumento para o topo da stack
+ * \brief Head da função PUSH que se encontra em stack.c
  * @param s Pointer para a stack principal
  * @param a Valor que queremos colocar na stack
  */
 void PUSH(Tipoval a,SPointer s);
 
 /**
- * \brief Esta funçao que imprime todos os elementos da stack
+ * \brief Head da função print_stack que se encontra em stack.c
  * @param s Pointer para s stack principal
  */
 void print_stack(SPointer s);
 
 /**
- * \brief Esta funçao move o top do pointer da stack para uma posição especifica
+ * \brief Head da função MOVE que se encontra em stack.c
  * @param s Pointer para s stack principal
  * @param a Posição para onde vamos deslocar.
  */
 void MOVE (int a,SPointer s);
 
 /**
- * \brief Esta função serve para retirar um elemento numa posição espefica na stack.
+ * \brief Head da função MOVEPOP que se encontra em stack.c
  * @param a Posição onde queremos retirar o elemento.
  * @param s Pointer para a stack principal
  */
 Tipoval MOVEPOP (int a,SPointer s);
 
 /**
- *\brief Como o próprio nome indica cria uma stack 
+ *\brief Head da função criaStack que se encontra em stack.c
  * @param s Pointer para a stack
  * @param tamanho Tamanho da stack que queremos criar
- * return s Retorna a stack criada
  */
 SPointer criaStack(SPointer s,int tamanho);
 
 /**
- * \brief Esta função copia a stack 
+ * \brief Head da função copystack que se encontra em stack.c
  * @param x Para onde vamos colocar a copia da stack
  * @param s Pointer para a stack principal
  */

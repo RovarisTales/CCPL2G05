8e1685c793074095e27ff9159dfe4778f022647a
Tsushiko <79994194+Tsushiko@users.noreply.github.com>
2021-05-08 20:51:37 +0100

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,11 +1,36 @@
 /**
- * Este ficheiro é a head do ficheiro parser.c.
+ * @file É o ficheiro que contem a head do ficheiro parser.c
  *
  */
+
+/**
+ * \brief Estamos a definir um pointer para a stack
+ */
 typedef struct Stack *SPointer;
+
+/**
+ * \brief Estamos a definir uma struct para o Tipoval
+ */
 typedef struct Tipovalor Tipoval;
+/**
+ * \brief Head do comparatipo que se encontra no parser.c
+ * @param a Tipo da primeira variavel
+ * @param b Tipo da segunda variavel
+ */
 char comparatipo (char a, char b);
+
+/**
+ * \brief Head do convertlong que se encontra no parser.c
+ * @param x Variavel que queremos converter
+ * @param a Tipo de x
+ */
 double convertelong (double x, char a);
+
+/**
+ * \brief Head do parser que se encontra no parser.c
+ * @param line A linha que foi lida na main para realizar o parser.
+ * @param alfabeto Alfabeto em maiuscula que corresponde ao codigo ascii (65-90)
+ */
 SPointer parser (char *line, Tipoval *alfabeto);
 
 
